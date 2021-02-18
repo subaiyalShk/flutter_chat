6cf8e3eddb20679d8949334a2f4b28afe34c4e09
subaiyal <subaiyalshk@gmail.com>
2021-02-18 11:38:50 -0800

    first commit

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../widgets/auth/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
+  void _submitAuthForm(
+      String email, String password, String username, bool isLogin) async {
+    AuthResult authResult;
+    try {
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      }
+    } on PlatformException catch (err) {
+      var message = 'An error occured, please check your credentials.';
+      if (err.message != null) {
+        message = err.message;
+      }
+
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text(message),
+        backgroundColor: Theme.of(context).errorColor,
+      ));
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AuthForm(_submitAuthForm),
+    );
+  }
+}
